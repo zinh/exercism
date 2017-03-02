00d14450b902e51f90a2ce5754dd1d4aae9d9bfc
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-03 08:37:23 +0900

    solution for hamming distance

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,18 @@
 package hamming
+import "errors"
 
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+  distance := 0
+  bs,as := []rune(b), []rune(a)
+  if len(as) != len(bs){
+    return -1, errors.New("length mismatch")
+  }
+  for index, c := range as {
+    if bs[index] != c{
+      distance += 1
+    }
+  }
+  return distance, nil
 }
