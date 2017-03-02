e3d6a551f2578d97154d721ee8b206a29a6977c0
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-03 08:05:27 +0900

    solution for gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,8 +1,12 @@
 // Package clause.
 package gigasecond
 
+import ("time")
+
 // Constant declaration.
 const testVersion = 4 // find the value in gigasecond_test.go
 
 // API function.  It uses a type from the Go standard library.
-func AddGigasecond(time.Time) time.Time
+func AddGigasecond(t time.Time) time.Time {
+  return t.Add(time.Duration(1000000000*time.Second))
+}
