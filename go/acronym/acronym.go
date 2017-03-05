55d3e131a1c67dd1e58427ef6454e3460b5a00b1
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-06 07:17:37 +0900

    solution for acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,5 +1,33 @@
 package acronym
 
+import (
+	"fmt"
+	"strings"
+)
+
 const testVersion = 2
 
-func Abbreviate(string) string
+func Abbreviate(term string) string {
+	result := ""
+	newWord := true
+	normalCase := false
+	for _, c := range term {
+		if c == ' ' || c == '-' {
+			newWord = true
+		} else {
+			if newWord {
+				result += strings.ToUpper(fmt.Sprintf("%c", c))
+			} else {
+				if c < 65 || c > 90 {
+					normalCase = true
+				} else {
+					if normalCase {
+						result += strings.ToUpper(fmt.Sprintf("%c", c))
+					}
+				}
+			}
+			newWord = false
+		}
+	}
+	return result
+}
