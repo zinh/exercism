d20f871334e85e45d8f83a8dcd6cf6107a9962b1
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-06 07:55:41 +0900

    pangram solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -2,4 +2,25 @@ package pangram
 
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(s string) bool {
+	charCount := make(map[rune]int)
+	for i := 0; i < 26; i++ {
+		charCount[rune('a'+i)] = 0
+	}
+
+	for _, c := range s {
+		if c >= 65 && c <= 90 {
+			c = c + 32
+		}
+		if charCount[c] == 0 {
+			charCount[c] += 1
+		}
+	}
+
+	for _, count := range charCount {
+		if count == 0 {
+			return false
+		}
+	}
+	return true
+}
