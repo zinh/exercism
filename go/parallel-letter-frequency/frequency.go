104a9be6598d36d7e81fbb7c40728167920f58f9
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-13 08:19:48 +0900

    frequency solution

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -1,5 +1,7 @@
 package letter
 
+const testVersion = 1
+
 type FreqMap map[rune]int
 
 func Frequency(s string) FreqMap {
@@ -8,4 +10,25 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+func ConcurrentFrequency(lines []string) FreqMap {
+	c := make(chan FreqMap)
+	result := FreqMap{}
+	for _, line := range lines {
+		go FrequencyWithChannel(line, c)
+	}
+	for i := 1; i <= 3; i++ {
+		select {
+		case r := <-c:
+			for char, count := range r {
+				result[char] += count
+			}
+		}
+	}
+	return result
+}
+
+func FrequencyWithChannel(s string, c chan FreqMap) {
+	c <- Frequency(s)
 }
