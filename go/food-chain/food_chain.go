23be6b43ad89977790d093e0220a058800c12378
Vinh Nguyen <vinhnx@zigexn.vn>
2017-03-16 10:41:12 +0900

    food_chain method init

---
+++ go/food-chain/food_chain.go
@@ -0,0 +1,15 @@
+package foodchain
+
+const testVersion = 3
+
+func Verse(verse int) string {
+  return ""
+}
+
+func Verses(from int, to int) string {
+  return ""
+}
+
+func Song() string {
+  return ""
+}
