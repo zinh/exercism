5639e1cadc367bee936e9a487cab26a23d8148aa
Vinh Nguyen <vinhnx@zigexn.vn>
2017-03-17 12:11:51 +0900

    crypto_square

---
+++ go/crypto-square/crypto_square.go
@@ -0,0 +1,13 @@
+package cryptosquare
+
+const testVersion = 2
+
+func Encode(s string) string {
+}
+
+func stripPunctuation(s string) string {
+  // Lower casse
+  // Remove punctuation
+  for _, c := range s {
+  }
+}
