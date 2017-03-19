630f1f5708075491b37a6a20e0bcbeda62c3ec0f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-19 21:26:38 +0900

    anagram solution

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,45 +1,33 @@
 package anagram
-import "fmt"
+
+import "strings"
 
 const testVersion = 1
 
 func Detect(s string, dic []string) []string {
-  result := []string{}
-  for _, word := range dic {
-    if isAnagram(s, word) {
-      result = append(result, word)
-    }
-  }
-  return result
+	result := []string{}
+	for _, word := range dic {
+		if isAnagram(strings.ToLower(s), strings.ToLower(word)) {
+			result = append(result, strings.ToLower(word))
+		}
+	}
+	return result
 }
 
 func isAnagram(s string, d string) bool {
-  // fmt.Printf("Source: %s, Dest: %s", s, d)
-  // fmt.Printf(" Source: %s, Dest: %s\n", sortString(s), sortString(d))
-  return sortString(s) == sortString(d)
+	return (s != d) && (sortString(s) == sortString(d))
 }
 
 func sortString(s string) string {
-  fmt.Println(s)
-  sortedArr := []rune{}
-  for _, char := range s {
-    fmt.Printf("\nCurrent: %s, new char: %s\n", string(sortedArr), string(char))
-    i := len(sortedArr) - 1
-    for ;i > 0; i-- {
-      if sortedArr[i] <= char {
-        break
-      }
-    }
-    if i < 0 || i >= len(sortedArr)-1 {
-      sortedArr = append(sortedArr, char)
-    } else {
-      halfTail := sortedArr[i:]
-      halfHead := sortedArr[0:i]
-      fmt.Printf("i = %d, HalfHead: %s, HalfTail: %s\n", i, string(halfHead), string(halfTail))
-      //tmp := append(halfHead, char)
-      fmt.Println(halfHead, char, halfTail)
-      //sortedArr = append(tmp, halfTail...)
-    }
-  }
-  return string(sortedArr)
+	r := []rune(s)
+	for i := 0; i < len(r); i++ {
+		min := i
+		for j := i; j < len(r); j++ {
+			if r[min] > r[j] {
+				min = j
+			}
+		}
+		r[i], r[min] = r[min], r[i]
+	}
+	return string(r)
 }
