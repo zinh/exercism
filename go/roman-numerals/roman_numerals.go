01406aa8978b27af7cd265db9772687edf4343dd
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-20 09:16:19 +0900

    roman numerals

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,14 @@
+package romannumerals
+//import "errors"
+
+var dict = map[int]string {
+  1: 'I',
+  5: 'V',
+  10: 'X',
+  50: 'L',
+  100: 'C',
+  500: 'D',
+  1000: 'M'
+}
+func ToRomanNumeral(n int) (string, error){
+}
