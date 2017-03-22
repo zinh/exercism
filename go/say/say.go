45e75bbb841b702c928e3a9cd7fd4a7b6d88391f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-03-23 07:47:09 +0900

    Update dictionary

--- go/say/say.go
+++ go/say/say.go
@@ -2,6 +2,11 @@ package say
 
 const testVersion = 1
 
+func init(){
+  var ones = []string{ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }
+  var twos = []string{ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" }
+  var tens []string{ "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety" }
+}
+
 func Say(n uint64) string {
-  return ""
 }
