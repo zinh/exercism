8b555f328e413c0f640e4528a0df5121d3336cee
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-10 07:20:42 +0900

    Bob solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,30 @@
 module Bob (responseFor) where
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | allUpper xs = "Whoa, chill out!"
+  | endWithQuestion xs = "Sure."
+  | allNonCharacter xs = "Fine. Be that way!"
+  | otherwise = "Whatever."
+
+allUpper :: String -> Bool
+allUpper [] = True
+allUpper (x:xs) 
+  | x >= 'a' && x <= 'z' = False
+  | otherwise = allUpper xs
+
+endWithQuestion :: String -> Bool
+endWithQuestion [] = False
+endWithQuestion "?" = True
+endWithQuestion (x:xs) = endWithQuestion xs
+
+endWithExclamation :: String -> Bool
+endWithExclamation [] = False
+endWithExclamation "!" = True
+endWithExclamation (x:xs) = endWithExclamation xs
+
+allNonCharacter :: String -> Bool
+allNonCharacter [] = True
+allNonCharacter (x:xs)
+  | x >= 'a' && x <= 'z' || x >= 'A' && x <= 'B' = False
+  | otherwise = allNonCharacter xs
