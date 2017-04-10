83b509f25ee761f00c87554381d8760aaf5f4eb4
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-11 07:45:35 +0900

    bob solution

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,16 +2,34 @@ module Bob (responseFor) where
 
 responseFor :: String -> String
 responseFor xs
+  | length(trim xs) == 0 = "Fine. Be that way!"
+  | onlyNumber xs = "Whatever."
   | allUpper xs = "Whoa, chill out!"
-  | endWithQuestion xs = "Sure."
+  | endWithQuestion(trim xs) = "Sure."
   | allNonCharacter xs = "Fine. Be that way!"
   | otherwise = "Whatever."
+  where trim = unwords . words
+
+onlyNumber :: String -> Bool
+onlyNumber [] = True
+onlyNumber (x:xs)
+  | x == '!' || x == ' ' || x == ',' || x >= '0' && x <= '9' = onlyNumber xs
+  | otherwise = False
+
+containUpper :: String -> Bool
+containUpper [] = False
+containUpper (x:xs)
+  | x >= 'A' && x <= 'Z' || x == '!' = True
+  | otherwise = containUpper xs
+
+containNonUpper :: String -> Bool
+containNonUpper [] = False
+containNonUpper (x:xs)
+  | x >= 'a' && x <= 'z' = True
+  | otherwise = containNonUpper xs
 
 allUpper :: String -> Bool
-allUpper [] = True
-allUpper (x:xs) 
-  | x >= 'a' && x <= 'z' = False
-  | otherwise = allUpper xs
+allUpper xs = (containUpper xs) && not (containNonUpper xs)
 
 endWithQuestion :: String -> Bool
 endWithQuestion [] = False
@@ -24,6 +42,8 @@ endWithExclamation "!" = True
 endWithExclamation (x:xs) = endWithExclamation xs
 
 allNonCharacter :: String -> Bool
+allNonCharacter xs
+  | length xs == 0 = True
 allNonCharacter [] = True
 allNonCharacter (x:xs)
   | x >= 'a' && x <= 'z' || x >= 'A' && x <= 'B' = False
