fd5761cc901d569b5e0631961a9dee6c3989e914
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-12 08:05:06 +0900

    run length encoding solution

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,26 @@
 module RunLength (decode, encode) where
 
+import Data.Char
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode encodedText = decode' encodedText 0 []
+
+decode' :: String -> Int -> String -> String
+decode' [] _ result = result
+decode' (x:xs) n result
+  | isNumber x = decode' xs (n * 10 + digitToInt x) result
+  | n == 0 = decode' xs 0 (result ++ [x])
+  | otherwise = decode' xs 0 (result ++ replicate n x)
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode [] = []
+encode (x:xs) = encode' xs x 1 []
+
+encode' :: String -> Char -> Int -> String -> String
+encode' [] currentChar currentCount result
+  | currentCount == 1 = result ++ [currentChar]
+  | otherwise = result ++ show currentCount ++ [currentChar]
+encode' (x:xs) currentChar currentCount result
+  | x == currentChar = encode' xs currentChar (currentCount + 1) result
+  | currentCount == 1 = encode' xs x 1 (result ++ [currentChar])
+  | otherwise = encode' xs x 1 (result ++ show currentCount ++ [currentChar])
