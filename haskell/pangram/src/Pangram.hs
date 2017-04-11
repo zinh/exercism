2645074fd5dfb2896242d811366cff5f7935d9cb
Vinh Nguyen <vinhnx@zigexn.vn>
2017-04-11 10:17:06 +0900

    refactor pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -3,15 +3,6 @@ module Pangram (isPangram) where
 import Data.Char
 
 isPangram :: String -> Bool
-isPangram = isPangram' . convertUpper
-
-isPangram' :: String -> Bool
-isPangram' text = 
-  let result = foldr (\c lst -> filter (c/=) lst) ['a'..'z'] text
+isPangram text = 
+  let result = foldr (\c lst -> filter (toLower(c)/=) lst) ['a'..'z'] text
   in length(result) == 0
-
-convertUpper :: String -> String
-convertUpper = foldr f []
-  where f c str
-          | c >= 'A' && c <= 'Z' = (chr $ (ord 'a') + (ord c) - (ord 'A')) : str
-          | otherwise = c:str
