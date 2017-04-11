4abc4d3f33411047bc4cf17b4584e980b102fdc1
Vinh Nguyen <vinhnx@zigexn.vn>
2017-04-11 10:12:35 +0900

    pangram solution

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,17 @@
 module Pangram (isPangram) where
 
+import Data.Char
+
 isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+isPangram = isPangram' . convertUpper
+
+isPangram' :: String -> Bool
+isPangram' text = 
+  let result = foldr (\c lst -> filter (c/=) lst) ['a'..'z'] text
+  in length(result) == 0
+
+convertUpper :: String -> String
+convertUpper = foldr f []
+  where f c str
+          | c >= 'A' && c <= 'Z' = (chr $ (ord 'a') + (ord c) - (ord 'A')) : str
+          | otherwise = c:str
