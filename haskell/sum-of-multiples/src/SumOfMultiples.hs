3b12c9eca68d96656a9fb2ced5ef35fa015455d7
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-13 08:22:54 +0900

    refactor

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -2,6 +2,4 @@ module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum $ filter (`divisible` factors) [1..(limit - 1)]
-
-divisible :: Integer -> [Integer] -> Bool
-divisible num = any (\factor -> num `mod` factor == 0)
+  where divisible num = any (\factor -> num `mod` factor == 0)
