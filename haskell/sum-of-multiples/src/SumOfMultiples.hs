816effa56169dce7873f2e915599c53169ef7882
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-13 08:22:05 +0900

    sum of multiples solution

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum $ filter (`divisible` factors) [1..(limit - 1)]
+
+divisible :: Integer -> [Integer] -> Bool
+divisible num = any (\factor -> num `mod` factor == 0)
