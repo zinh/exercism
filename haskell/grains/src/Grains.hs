6b1b95fd1241e3612e631171b415f7268b03a18f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-14 07:31:28 +0900

    grains solution

--- haskell/grains/src/Grains.hs
+++ haskell/grains/src/Grains.hs
@@ -1,7 +1,10 @@
 module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n
+  | n >= 1 && n <= 64 = Just (2^(n - 1))
+  | otherwise = Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = foldr f 0 [0..63]
+  where f n sum = sum + 2^n
