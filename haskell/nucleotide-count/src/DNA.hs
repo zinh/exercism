2d60092a6c42c2d6f91293f6b66f53f63efa3895
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-17 08:18:19 +0900

    nucleotide count solution

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,6 +1,12 @@
 module DNA (nucleotideCounts) where
 
-import Data.Map (Map)
+import Data.Map (Map, fromList, lookup, insert)
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = foldr nucleotideCounts' (Right (fromList [('A',0),('C',0),('G',0),('T',0)])) xs
+
+nucleotideCounts' :: Char -> Either String (Map Char Int) -> Either String (Map Char Int)
+nucleotideCounts' _ (Left s) = Left s
+nucleotideCounts' x (Right m) = case Data.Map.lookup x m of
+                                  Just n -> Right (Data.Map.insert x (n + 1) m)
+                                  otherwise -> Left "Invalid Strand"
