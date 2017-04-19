e2b45c3c53fe6ea2ffd50ab378433e1e3d7f808f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-04-20 08:00:20 +0900

    etl solution

--- haskell/etl/src/ETL.hs
+++ haskell/etl/src/ETL.hs
@@ -1,6 +1,9 @@
 module ETL (transform) where
 
-import Data.Map (Map)
+import Data.Map (Map, toList, fromList)
+import Data.Char (toLower)
 
 transform :: Map a String -> Map Char a
-transform legacyData = error "You need to implement this function."
+transform legacyData = Data.Map.fromList $ foldr f [] (Data.Map.toList legacyData)
+  where f (k, x:xs) memo = f (k, xs) ((toLower x, k):memo)
+        f (_, []) memo = memo
