bbdecbb1870253d1b48b4b427027dd23509eba03
Vinh Nguyen <xuanvinhct@gmail.com>
2017-09-28 10:00:50 +0700

    solution for two fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -4,12 +4,16 @@
 // Package twofer should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
 package twofer
+import "fmt"
 
 // ShareWith needs a comment documenting it.
-func ShareWith(string) string {
+func ShareWith(name string) string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers will protest!
-	return ""
+        if name != "" {
+          return fmt.Sprintf("One for %s, one for me.", name)
+        }
+        return "One for you, one for me."
 }
