da7b2d761790343eac1d09c1ea2a0b810f853e0a
Vinh Nguyen <xuanvinhct@gmail.com>
2017-10-16 17:19:03 +0700

    update circular buffer

--- go/circular-buffer/circular_buffer.go
+++ go/circular-buffer/circular_buffer.go
@@ -1,17 +1,25 @@
 package circular
 
+import ("errors")
+
 const testVersion = 4
 
 type Buffer struct {
-  content []int
-  pointer int
+  content []byte
+  current int
+  first int
 }
 
 func NewBuffer(size int) *Buffer {
-  return Buffer{content: []int{}, 0}
+  return &Buffer{content: make([]byte, size), current: 0, first: 0}
 }
 
-func (*Buffer) ReadByte() (byte, error)
+func (b *Buffer) ReadByte() (byte, error) {
+  if b.current == b.first {
+    return 0, errors.New("Empty buffer")
+  }
+  return b.content[current], nil
+}
 
 func (*Buffer) WriteByte(c byte) error
 
