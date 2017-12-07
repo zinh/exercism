33bf5420cc3a561e9c1cd326b3b0d29ef9f8237f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-07 21:46:42 +0700

    finish reverse string in rust

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn reverse(input: &str) -> String {
     let mut result: String = String::from("");
     for c in input.chars() {
-        result.push(c);
+        result = c.to_string() + &result
     }
     result
 }
