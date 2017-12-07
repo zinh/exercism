58df5e10b8ff1852865aa25abdb1e12c22869406
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-07 21:04:52 +0700

    raindrop solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,2 +1,19 @@
-pub fn raindrops(n: i32) -> &str {
+pub fn raindrops(n: i32) -> String {
+    let mut result: String = String::from("");
+
+    if n % 3 == 0 {
+        result.push_str("Pling");
+    } 
+
+    if n % 5 == 0 {
+        result.push_str("Plang");
+    } 
+
+    if n % 7 == 0 {
+        result.push_str("Plong");
+    }
+    if result == "" {
+        result = n.to_string();
+    }
+    result
 }
