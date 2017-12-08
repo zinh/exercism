7c535b9ba93a12a058544e5908a7d16b8706baa5
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-08 15:53:22 +0700

    solution for bob problem

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,40 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!()
+    if is_blank(message.trim()) {
+        return "Fine. Be that way!";
+    }
+    if is_contain_letter(message.trim()) && !is_contain_lowercase(message.trim()) {
+        return "Whoa, chill out!";
+    }
+
+    if is_question(message.trim()) {
+        return "Sure.";
+    }
+
+    return "Whatever.";
+}
+
+pub fn is_question(message: &str) -> bool {
+    message.ends_with("?")
+}
+
+pub fn is_blank(message: &str) -> bool {
+    message == ""
+}
+
+pub fn is_contain_lowercase(message: &str) -> bool {
+    for c in message.chars() {
+        if c.is_lowercase() {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn is_contain_letter(message: &str) -> bool {
+    for c in message.chars() {
+        if c.is_alphabetic() {
+            return true;
+        }
+    }
+    return false;
 }
---
+++ rust/bob/src/main.rs
@@ -0,0 +1,5 @@
+extern crate bob;
+
+fn main(){
+    println!("{}", bob::is_contain_lowercase("WHAT THE HELL WERE YOU THINKING?"));
+}
