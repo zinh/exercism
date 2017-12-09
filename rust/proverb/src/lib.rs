dc2a5c5891eb5161a46f7121198fbb1fff2a183f
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-09 17:08:01 +0700

    proverb solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn build_proverb(list: Vec<&str>) -> String {
-    unimplemented!()
+    let mut idx: usize = 0;
+    let mut result: Vec<String> = Vec::new();
+    if list.len() == 0 {
+        return String::new()
+    }
+    loop {
+        let current_item = list[idx];
+        if idx == list.len() - 1 {
+            result.push(format!("And all for the want of a {}.", list[0]));
+            break;
+        } else {
+            result.push(format!("For want of a {} the {} was lost.", current_item, list[idx + 1]));
+        }
+        idx = idx + 1;
+    }
+    return result.join("\n");
 }
