bba7426bbdac8c6b66c1dc2b8821a53e009cedd8
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-09 15:22:04 +0700

    solution for beer song

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,36 @@
 pub fn verse(n: i32) -> String {
-    unimplemented!()
+    format!("{} of beer on the wall, {} of beer.\n{}, {} of beer on the wall.\n", 
+            pluralize(n, true), 
+            pluralize(n, false), 
+            counter(n),
+            pluralize(n - 1, false))
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!()
+    let mut verses: Vec<String> = Vec::new();
+    for number in (end..start + 1).rev() {
+        verses.push(verse(number))
+    }
+    verses.join("\n")
+}
+
+fn pluralize(num: i32, capitalize: bool) -> String {
+    match num {
+        -1 => String::from("99 bottles"),
+        0 => if capitalize {
+            String::from("No more bottles")
+        } else {
+            String::from("no more bottles")
+        },
+        1 => String::from("1 bottle"),
+        _ => format!("{} bottles", num),
+    }
+}
+
+fn counter(num: i32) -> String {
+    match num {
+        0 => String::from("Go to the store and buy some more"),
+        1 => String::from("Take it down and pass it around"),
+        _ => String::from("Take one down and pass it around"),
+    }
 }
