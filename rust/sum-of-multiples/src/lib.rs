d5a2da42e36f6dbd1d179d92914674a6d6c17625
Vinh Nguyen <xuanvinhct@gmail.com>
2017-12-10 14:20:03 +0700

    sum of multiplies

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!()
+    let mut results: Vec<u32> = Vec::new();
+    for factor in factors {
+        for i in 1..limit {
+            if i % factor == 0 && !exists(&results, i) {
+                results.push(i);
+            }
+        }
+    }
+    return results.iter().fold(0, |acc, x| acc + x);
+}
+
+fn exists(arr: &Vec<u32>, item: u32) -> bool {
+    for &i in arr {
+        if item == i {
+            return true;
+        }
+    }
+    return false;
 }
---
+++ rust/sum-of-multiples/src/main.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let a: Vec<u32> = vec![1,2,3];
+    for i in a {
+        println!("{}", i);
+    }
+}
