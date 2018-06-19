42e8fbffe5c169868a7d933d1f8444e866cccb73
Vinh Nguyen <xuanvinhct@gmail.com>
2018-06-19 22:50:27 +0900

    Isogram solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,9 @@
 def is_isogram(string):
-    pass
+    k = {}
+    for c in string.lower():
+        if c == ' ' or c == '-':
+            continue
+        if k.get(c):
+            return False
+        k[c] = True
+    return True
