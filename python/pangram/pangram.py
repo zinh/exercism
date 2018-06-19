1db0e9058d6ef46fc86f70a2a2f25e08347e238e
Vinh Nguyen <xuanvinhct@gmail.com>
2018-06-19 23:04:24 +0900

    Pangram solution

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
 def is_pangram(sentence):
-    pass
+    if sentence == "":
+        return False
+    h = {}
+    for c in range(ord('a'), ord('z')):
+        h[chr(c)] = 0
+    for c in sentence.lower():
+        if h.get(c) == 0:
+            h[c] += 1
+    for c, count in h.items():
+        if count == 0:
+            return False
+    return True
