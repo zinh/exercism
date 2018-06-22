3813f6486c2e370d500c56df2d5a566415b98b66
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-06-22 12:39:38 +0900

    hamming distance

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,7 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Incompatible length")
+    distance = 0
+    for idx, nu in enumerate(strand_a):
+        distance += 1 if nu != strand_b[idx] else 0
+    return distance
