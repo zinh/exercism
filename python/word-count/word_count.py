62302080e772a37f4077e39a2ed3b11f424fe8b2
Vinh Nguyen <xuanvinhct@gmail.com>
2018-06-24 16:54:27 +0900

    python word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,27 @@
+import re
+
 def word_count(phrase):
-    pass
+    result = {}
+    current_word = ""
+    phrase = phrase.lower()
+    for i, c in enumerate(phrase):
+        if delimiter(c):
+            current_word = trim(current_word)
+            if current_word != '':
+                result[current_word] = result.get(current_word, 0) + 1
+            current_word = ''
+        else:
+            current_word += c
+    if current_word != '':
+        current_word = trim(current_word)
+        result[current_word] = result.get(current_word, 0) + 1
+    print(result)
+    return result
+
+def delimiter(c):
+    return True if c in ' :.,\n_\t' else False
+
+# remove leading puntuation
+def trim(s):
+    s1 = re.sub(r'[^0-9A-Za-z]+\Z', '', s)
+    return re.sub(r'^[^0-9A-Za-z]+', '', s1)
