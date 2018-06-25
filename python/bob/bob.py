2fbaaaa7a4da97fd9bfbcd54e3c5ed8a6fdbdb2c
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-06-25 15:58:06 +0900

    bob in py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,27 @@
 def hey(phrase):
-    pass
+    phrase = phrase.strip()
+    if phrase == '':
+        return 'Fine. Be that way!'
+    if contain_character(phrase):
+        if end_with_question(phrase) and all_capital(phrase):
+            return "Calm down, I know what I'm doing!"
+        elif all_capital(phrase):
+            return 'Whoa, chill out!'
+    if end_with_question(phrase):
+        return 'Sure.'
+    return 'Whatever.'
+
+def end_with_question(phrase):
+    return phrase.endswith('?')
+
+def all_capital(phrase):
+    for c in phrase:
+        if c >= 'a' and c <= 'z':
+            return False
+    return True
+
+def contain_character(phrase):
+    for c in phrase:
+        if 'a' <= c <= 'z' or 'A' <= c <= 'Z':
+            return True
+    return False
