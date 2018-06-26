a4117030b12a3999c52e7d65b99a50b6357376b5
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-06-26 17:47:14 +0900

    Yatch solution

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -14,25 +14,30 @@ BIG_STRAIGHT = 10
 CHOICE = 11
 
 def score(dice, category):
-    if category == YACHT and is_yatch(dice):
+    counted_dice = count_dice(dice)
+    if category == YACHT and is_yatch(counted_dice):
+        return 50
+    if category == FULL_HOUSE and full_house(counted_dice):
         return sum(dice)
-    if category == FULL_HOUSE and full_house(dice):
-        return sum(dice)
-    if category == FOUR_OF_A_KIND and four_kind(dice):
-        return sum(dice)
+    if category == FOUR_OF_A_KIND and four_kind(counted_dice):
+        val = counted_dice[4] if 4 in counted_dice else counted_dice[5]
+        return val * 4
     if category == LITTLE_STRAIGHT and little_straight(dice):
         return 30
     if category == BIG_STRAIGHT and big_straight(dice):
         return 30
     if category == CHOICE:
         return sum(dice)
+    if category in [ONES, TWOS, THREES, FOURS, FIVES, SIXES] and is_number(dice, category):
+        return category * dice.count(category)
     return 0
 
+
+def count_dice(dice):
+    return {dice.count(x): x for x in dice}
+
 def is_yatch(dice):
-    for val in dice:
-        if val != dice[0]:
-            return False
-    return True
+    return 5 in dice
 
 def is_number(dice, number):
     for val in dice:
@@ -41,10 +46,10 @@ def is_number(dice, number):
     return False
 
 def full_house(dice):
-    return {dice.count(x) for x in set(dice)} == {2, 3}
+    return 2 in dice and 3 in dice
 
 def four_kind(dice):
-    return {x: dice.count(x) for x in set(dice)} == {4, 1}
+    return 4 in dice or 5 in dice
 
 def little_straight(dice):
     return set(dice) == {1,2,3,4,5}
