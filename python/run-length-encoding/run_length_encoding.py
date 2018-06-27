09da54826e12854727a19ed1bc4fd79fa0621b80
Vinh Nguyen <xuanvinhct@gmail.com>
2018-06-27 09:28:11 +0900

    py run_length_encoding

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,15 @@
 def decode(string):
-    pass
-
+    return string
 
 def encode(string):
-    pass
+    current_count = 0
+    current_c = ''
+    result = ''
+    for c in string:
+        if c == current_c:
+            current_count += 1
+        else:
+            result += str(current_count) + current_c if current_count > 1 else current_c
+            current_c = c
+            current_count = 1
+    return result
