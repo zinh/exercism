2989cd8623ec9df56b96c9b7b3bb7c5e249dcc5f
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-06-27 14:29:47 +0900

    Run lenght encoding py

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,15 +1,30 @@
 def decode(string):
-    return string
+    result = ""
+    current_count = ""
+    i = 0
+    while i < len(string):
+        c = string[i]
+        if '0' <= c <= '9':
+            current_count += c
+        else:
+            if current_count == '':
+                result += c
+            else:
+                result += int(current_count) * c
+            current_count = ''
+        i += 1
+    return result
 
 def encode(string):
     current_count = 0
     current_c = ''
     result = ''
-    for c in string:
-        if c == current_c:
-            current_count += 1
-        else:
+    string += ' '
+    for (idx, c) in enumerate(string):
+        if c != current_c or idx == len(string) - 1:
             result += str(current_count) + current_c if current_count > 1 else current_c
             current_c = c
             current_count = 1
+        else:
+            current_count += 1
     return result
