273dd055c269fb65d43a102fef0dbc608e86beef
Vinh Nguyen <xuanvinhct@gmail.com>
2018-07-07 15:51:45 +0900

    Python: rotational cipher solution

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,10 @@
 def rotate(text, key):
-    pass
+    return ''.join(list(map(lambda char: cipher(char, key), text)))
+
+def cipher(char, key):
+    if (char >= 'a' and char <= 'z'):#  or (char >= 'A' and char <= 'Z'):
+        return chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+    elif char >= 'A' and char <= 'Z':
+        return chr((ord(char) - ord('A') + key) % 26 + ord('A'))
+    else:
+        return char
