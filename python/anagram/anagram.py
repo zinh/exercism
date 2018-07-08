9ffdfae82b768665491903fc7c395cba78818c8d
Vinh Nguyen <xuanvinhct@gmail.com>
2018-07-08 16:05:39 +0900

    Python anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,7 @@
 def detect_anagrams(word, candidates):
-    pass
+    sorted_word = ''.join(sorted(list(word.lower())))
+    return [candidate for candidate in candidates if word.lower() != candidate.lower() and is_anagram(sorted_word, candidate.lower())]
+
+def is_anagram(word, candidate):
+    sorted_word = ''.join(sorted(list(candidate)))
+    return word == sorted_word
