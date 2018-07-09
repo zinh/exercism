75eb588df8d89bf7c5fff0c719ada5176ba34105
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-09 10:29:24 +0900

    Python series solution

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,11 @@
 def slices(series, length):
-    pass
+    if length <= 0 or len(series) < length:
+        raise ValueError("Invalid length")
+    start_pos = 0
+    end_pos = start_pos + length
+    results = []
+    while end_pos <= len(series):
+        results.append(series[start_pos:end_pos])
+        start_pos += 1
+        end_pos = start_pos + length
+    return results
