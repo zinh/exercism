c22aae23c1504436ac10028aa28a88145edd5baf
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-11 15:55:55 +0900

    Robot simulator

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,10 +1,10 @@
 from math import sin, cos, pi
 # Globals for the bearings
 # Change the values as you see fit
-EAST = -pi/2
-NORTH = 0
-WEST = pi/2
-SOUTH = pi
+EAST = (1, 0)
+NORTH = (0, 1)
+WEST = (-1, 0)
+SOUTH = (0, -1)
 
 
 class Robot(object):
@@ -12,24 +12,30 @@ class Robot(object):
         self.bearing = bearing
         self.x = x
         self.y = y
-        self.rotate_matrix = [
-                [int(cos(self.bearing)), -int(sin(self.bearing))],
-                [int(sin(self.bearing)), int(cos(self.bearing))]]
-        print(self.rotate_matrix)
 
     @property
     def coordinates(self):
-        return (self.rotate_matrix[0][0] * self.x + self.rotate_matrix[0][1] * self.y,
-        self.rotate_matrix[1][0] * self.x + self.rotate_matrix[1][1] * self.y)
+        return (self.x, self.y)
 
     def turn_right(self):
-        self.x = self.x + 1
+        r = self.rotate(-pi/2)
+        bearing = self.bearing
+        self.bearing = self.mul(r, bearing)
 
     def turn_left(self):
-        self.x = self.x - 1
+        r = self.rotate(pi/2)
+        bearing = self.bearing
+        self.bearing = self.mul(r, bearing)
 
     def advance(self):
-        self.y = self.y + 1
+        self.x = self.x + self.bearing[0]
+        self.y = self.y + self.bearing[1]
+
+    def rotate(self, deg):
+        return ([int(cos(deg)), -int(sin(deg))], [int(sin(deg)), int(cos(deg))])
+
+    def mul(self, r, v):
+        return (r[0][0] * v[0] + r[0][1] * v[1], r[1][0] * v[0] + r[1][1] * v[1])
 
     def simulate(self, path):
         for direction in path:
