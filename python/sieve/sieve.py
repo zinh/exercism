ab480a22baa63d6bbb89fd75f68d540e7925a286
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-17 10:26:20 +0900

    Py: sieve solution

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,10 @@
 def sieve(limit):
-    pass
+    if limit == 1:
+        return []
+    s = [True] * (limit + 1)
+    for i in range(2, limit + 1):
+        if not s[i]:
+            continue
+        for j in range(2, (limit // i) + 1):
+            s[i * j] = False
+    return [i for i in range(2, limit + 1) if s[i]]
