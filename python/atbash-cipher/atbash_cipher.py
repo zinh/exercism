5410657bba731f91bd0b00e52e97dfed6b81d435
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-18 12:50:41 +0900

    Atbash

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,19 @@
-def encode(plain_text):
-    pass
+from functools import reduce
 
+chars = list(range(ord('a'), ord('z') + 1))
+
+def encode(plain_text):
+    return ''.join(reduce(encode_char, plain_text.lower(), []))
+
+def encode_char(memo, c):
+    if c >= 'a' and c <= 'z':
+        c_encoded = chr(chars[96 - ord(c)])
+        memo.append(c_encoded)
+        return memo
+    else:
+        return memo
 
 def decode(ciphered_text):
-    pass
+    return ''.join(reduce(encode_char, ciphered_text.lower(), []))
+
+print(encode('a'))
