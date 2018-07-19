05446ef91edb780b4555711f6def4935dad46cff
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-19 10:18:45 +0900

    Atbast cipher solution

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -3,17 +3,16 @@ from functools import reduce
 chars = list(range(ord('a'), ord('z') + 1))
 
 def encode(plain_text):
-    return ''.join(reduce(encode_char, plain_text.lower(), []))
+    s = reduce(encode_char, plain_text.lower(), [])
+    return ' '.join([''.join(s[i:i + 5]) for i in range(0, len(s), 5)])
 
 def encode_char(memo, c):
     if c >= 'a' and c <= 'z':
         c_encoded = chr(chars[96 - ord(c)])
         memo.append(c_encoded)
-        return memo
-    else:
-        return memo
+    elif c >= '0' and c <= '9':
+        memo.append(c)
+    return memo
 
 def decode(ciphered_text):
     return ''.join(reduce(encode_char, ciphered_text.lower(), []))
-
-print(encode('a'))
