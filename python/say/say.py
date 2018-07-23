4e6ac1be44a978ac96f13b6c78d6df85bfc2ea3f
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-23 12:30:44 +0900

    Doing: python say exercise

--- python/say/say.py
+++ python/say/say.py
@@ -1,2 +1,18 @@
 def say(number):
-    pass
+    if number < 0:
+        raise ValueError('Invalid number')
+    return unit(number)
+
+def unit(number):
+    m = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four',
+            5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
+            10: 'ten', 11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+            15: 'fifteen'}
+    return m.get(number)
+
+def tenth(number):
+    m = {2: 'twenty', 3: 'thirty', 4: 'fourty', 5: 'fifty'}
+    if m.get(number):
+        return m.get(number)
+    elif 6 <= number <= 9:
+        return f'{unit(number)}ty'
