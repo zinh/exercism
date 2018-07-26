2726253a76a0a45bf609463bc669e17bf7a4ad9c
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-26 11:20:00 +0900

    Kindergarten solution

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,16 @@
 class Garden(object):
-    def __init__(self, diagram, students):
-        pass
+    m = {'R': 'Radishes', 'C': 'Clover', 'G': 'Grass', 'V': 'Violets'}
+
+    def __init__(self, diagram, students = None):
+        self.lines = [self.divide_str(line, 2) for line in diagram.split('\n')]
+        self.students = sorted(students, key=lambda x: x[0]) if students else students
+
+    def plants(self, student):
+        i = self.students.index(student) if self.students else ord(student[0]) - 65
+        return self.map_flower("".join([line[i] for line in self.lines]))
+
+    def divide_str(self, txt, str_len):
+        return [txt[i:i+str_len] for i in range(0, len(txt), str_len)]
+
+    def map_flower(self, flowers):
+        return [self.m[flower] for flower in flowers]
