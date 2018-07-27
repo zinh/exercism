925db9c6e151307983d4c2b3259b42617e23aca8
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-27 11:11:35 +0900

    Grade school solution

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,13 @@
 class School(object):
     def __init__(self):
-        pass
+        self.grade_book = {}
 
     def add_student(self, name, grade):
-        pass
+        self.grade_book[name] = grade
 
     def roster(self):
-        pass
+        return sorted(self.grade_book.keys(), key=lambda x: [str(self.grade_book[x]), x[0]])
 
     def grade(self, grade_number):
-        pass
+        names = [name for name, grade in self.grade_book.items() if grade == grade_number]
+        return sorted(names, key=lambda x: x[0])
