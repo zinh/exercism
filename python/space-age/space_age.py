1aba6ca89608c05f22235bef4aad1ee461eccf6b
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-30 15:25:47 +0900

    Python: space age solution

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,3 +1,36 @@
 class SpaceAge(object):
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+
+    def on_earth(self):
+        return self.format_number(self.seconds / 31557600)
+
+    def on_earth_original(self):
+        return self.seconds / 31557600
+
+    def on_mercury(self):
+        return self.format_number(self.on_earth_original() / 0.2408467)
+
+    def on_venus(self):
+        return self.format_number(self.on_earth_original() / 0.61519726)
+
+    def on_mars(self):
+        return self.format_number(self.on_earth_original() / 1.8808158)
+
+    def on_jupiter(self):
+        return self.format_number(self.on_earth_original() / 11.862615)
+
+    def on_saturn(self):
+        return self.format_number(self.on_earth_original() / 29.447498)
+
+    def on_uranus(self):
+        return self.format_number(self.on_earth_original() / 84.016846)
+
+    def on_neptune(self):
+        return self.format_number(self.on_earth_original() / 164.79132)
+
+    def seconds(self):
+        return self.seconds
+
+    def format_number(self, number):
+        return round(number, 2)
