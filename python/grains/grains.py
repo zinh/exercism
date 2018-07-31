a0cd1266924ff8cd450b7824fd5cf756ee5ef316
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-07-31 11:55:11 +0900

    Python: grains solution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
+from functools import reduce
 def on_square(integer_number):
-    pass
+    if integer_number <= 0:
+        raise ValueError("Non-positive number")
+    if integer_number > 64:
+        raise ValueError("Too big number")
+    return 2**(integer_number - 1)
 
 
 def total_after(integer_number):
-    pass
+    if integer_number <= 0:
+        raise ValueError("Non-positive number")
+    if integer_number > 64:
+        raise ValueError("Too big number")
+    return reduce(lambda sum, el: sum + on_square(el), range(1, integer_number + 1), 0)
