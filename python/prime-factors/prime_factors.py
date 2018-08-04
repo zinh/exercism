5c8b5952382933c0dac6692d311eef86fad73922
Vinh Nguyen <xuanvinhct@gmail.com>
2018-08-04 14:59:39 +0900

    Prime factor solution

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,8 @@
+from math import sqrt
 def prime_factors(natural_number):
-    pass
+    if natural_number < 2:
+        return []
+    for i in range(2, int(sqrt(natural_number)) + 1):
+        if natural_number % i == 0:
+            return [i] + prime_factors(natural_number // i)
+    return [natural_number]
