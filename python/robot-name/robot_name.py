cd92af50c318e49e5647248cfdeb6cf22d6b030d
Vinh Nguyen <xuanvinhct@gmail.com>
2018-08-12 15:26:55 +0900

    Robot name solution

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,3 +1,15 @@
+import random
+from os import urandom
+
 class Robot(object):
     def __init__(self):
-        pass
+        self.assign_name()
+    def name(self):
+        return self.name
+    def reset(self):
+        self.assign_name()
+    def assign_name(self):
+        random.seed(urandom(5))
+        head = random.choices([chr(c) for c in range(ord('A'), ord('Z'))], k=2)
+        tail = random.choices([str(i) for i in range(0, 9)], k=3)
+        self.name = ''.join(head + tail)
