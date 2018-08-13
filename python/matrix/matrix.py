8266f4f0b2ffe2bdb7db0e176d54a7dc669f5a62
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-13 13:11:35 +0900

    Matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,17 @@
+from functools import reduce
+
 class Matrix(object):
     def __init__(self, matrix_string):
-        pass
+        rows = [list(map(lambda c: int(c), row.split(' '))) for row in matrix_string.split('\n')]
+        cols = [[None] * len(rows) for c in rows[0]]
+        for row_num, row in enumerate(rows):
+            for col_num, i in enumerate(row):
+                cols[col_num][row_num] = i
+        self.rows = rows
+        self.cols = cols
 
     def row(self, index):
-        pass
+        return self.rows[index]
 
     def column(self, index):
-        pass
+        return self.cols[index]
