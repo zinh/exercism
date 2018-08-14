4a97d3e449c0be78df4861d03152e6bf4caf4ad5
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-14 11:51:31 +0900

    Rail fence

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -1,6 +1,23 @@
 def encode(message, rails):
-    pass
+    rows = [[] for r in range(0, rails)]
+    r = 0
+    direction = 1
+    for idx, c in enumerate(message):
+        rows[r] += [c]
+        if r == rails - 1:
+            direction = -1
+        elif r == 0:
+            direction = 1
+        r = r + direction
+    return ''.join([''.join(row) for row in rows])
 
 
 def decode(encoded_message, rails):
-    pass
+    rows = []
+    for r in range(0, rails):
+        distance = 2 * (rails - 2) + 1 - 2*r
+        print(r, distance)
+        rows += [encoded_message[r::distance]]
+    return rows
+
+print(decode('WECRLTEERDSOEEFEAOCAIVDEN', 3))
