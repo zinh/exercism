071230f47633e584eb6ba6064370ab93e584e254
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-15 18:54:04 +0900

    Haskell: Perfect number

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -3,4 +3,4 @@ module PerfectNumbers (classify, Classification(..)) where
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify num = Just Perfect
