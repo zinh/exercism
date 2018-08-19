f7e735d04185f56f46fd0613838262d0dcbc9095
Vinh Nguyen <xuanvinhct@gmail.com>
2018-08-19 11:12:50 +0900

    perfect number

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,11 @@
+from functools import reduce
 def classify(number):
-    pass
+    if number <= 0:
+        raise ValueError("Non positive number")
+    aliquot = reduce(lambda s, d: s + d if number % d == 0 else s, range(1, number//2 + 1), 0)
+    if aliquot == number:
+        return 'perfect'
+    elif aliquot < number:
+        return 'deficient'
+    else:
+        return 'abundant'
