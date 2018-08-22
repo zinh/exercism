34a907465fbe2449825265f0d69cc82d5c093d9f
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-22 18:45:45 +0900

    Isogram

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -3,4 +3,11 @@ import qualified Data.Map as Map
 import Data.Char (toLower)
 
 isIsogram :: String -> Bool
-isIsogram str = fst $ foldl (\(result, acc) c -> if (toLower c) < 'a' || (toLower c) > 'z' then (True, acc) else if not result || Map.member (toLower c) acc then (False, acc) else (True, Map.insert (toLower c) True acc)) (True, Map.empty) str
+isIsogram str = fst $ foldl (\(result, acc) originalChar -> 
+  let c = toLower originalChar in
+  if c < 'a' || c > 'z' 
+    then (True, acc) 
+    else if not result || Map.member c acc 
+      then (False, acc) 
+      else (True, Map.insert c True acc)) 
+  (True, Map.empty) str
