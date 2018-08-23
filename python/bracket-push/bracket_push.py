ede375abd78de372bfd4f697d686a0c1863c446f
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-23 10:30:03 +0900

    Python bracket push solution

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -1,2 +1,15 @@
 def is_paired(input_string):
-    pass
+    pairs = {')': '(', '}': '{', ']': '['}
+    stack = []
+    for symbol in input_string:
+        for c in pairs.values():
+            if symbol == c:
+                stack.append(symbol)
+                break
+        for c in pairs:
+            if symbol == c and len(stack) > 0 and stack[-1] == pairs[c]:
+                stack.pop()
+                break
+            elif symbol == c:
+                return False
+    return True if len(stack) == 0 else False
