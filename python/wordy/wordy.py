b6e0dd0d759f34475c81cb4ca59244df11e0f77f
Vinh Nguyen <xuanvinhct@gmail.com>
2018-08-26 14:55:17 +0900

    Python wordy solution

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -1,2 +1,37 @@
+import re
+
 def calculate(question):
-    pass
+    numbers = re.findall(r'(-?[0-9]+)', question)
+    operators = re.findall(r'(plus|minus|multiplied|divided)', question)
+    expr = []
+    if len(numbers) == 0:
+        raise ValueError("Invalid question")
+    for idx, number in enumerate(numbers):
+        expr.append(int(number))
+        if idx < len(operators):
+            expr.append(operators[idx])
+    if len(expr) <= 1:
+        raise ValueError("Invalid question")
+    return evaluate(expr)
+
+def evaluate(expr):
+    stack = [expr[0]]
+    for el in expr[1:]:
+        if type(el) is int and type(stack[-1]) is not int:
+            result = calc(stack.pop(), stack.pop(), el)
+            stack.append(result)
+        else:
+            stack.append(el)
+    if len(stack) > 1:
+        raise ValueError('Invalid expression')
+    return stack[0]
+
+def calc(op, e1, e2):
+    if op == 'plus':
+        return e1 + e2
+    elif op == 'minus':
+        return e1 - e2
+    elif op == 'multiplied':
+        return e1 * e2
+    elif op == 'divided':
+        return e1 / e2
