59c87c364dfb8f61cb2937eb33e22f211739fa39
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-27 17:05:02 +0900

    Haskell raindrops solution

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -1,4 +1,14 @@
 module Raindrops (convert) where
+import Data.List (intercalate)
 
 convert :: Int -> String
-convert n = error "You need to implement this function."
+convert n = 
+  let divisions = [mapN d | d <- [3, 5, 7], mod n d == 0]
+  in case divisions of
+    [] -> show n
+    _ -> intercalate "" divisions
+
+mapN :: Int -> String
+mapN 3 = "Pling"
+mapN 5 = "Plang"
+mapN 7 = "Plong"
