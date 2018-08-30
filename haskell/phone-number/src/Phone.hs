9cd4695ba56f63afce18a21f7c0fce5941ae42f1
Vinh Nguyen <xuanvinhct@gmail.com>
2018-08-30 23:10:22 +0900

    Haskell phone numbers

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,4 +1,21 @@
 module Phone (number) where
 
 number :: String -> Maybe String
-number xs = error "You need to implement this function."
+number xs =
+  let numbers = [c | c <- xs, c >= '0' && c <= '9']
+  in isValid(numbers)
+
+isValid :: String -> Maybe String
+isValid numbers
+  | l < 10 || l > 11 = Nothing
+  | l == 11 && head(numbers) /= '1' = Nothing
+  | l == 11 = normalize (tail numbers)
+  | l == 10 = normalize numbers
+  | otherwise = Just numbers
+  where l = length(numbers)
+
+normalize :: String -> Maybe String
+normalize numbers
+  | head numbers == '1' || head numbers == '0' = Nothing
+  | numbers !! 3 == '1' || numbers !! 3 == '0' = Nothing
+  | otherwise = Just numbers
