1d236c36c7dba4f2dc44e477805764de614c6bdc
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-30 11:00:22 +0900

    Python triangle solution

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,17 @@
 def is_equilateral(sides):
-    pass
-
+    if not valid_triangle(sides):
+        return False
+    return sides[0] == sides[1] and sides[1] == sides[2]
 
 def is_isosceles(sides):
-    pass
-
+    if not valid_triangle(sides):
+        return False
+    return sides[0] == sides[1] or sides[1] == sides[2] or sides[2] == sides[0]
 
 def is_scalene(sides):
-    pass
+    if not valid_triangle(sides):
+        return False
+    return not is_equilateral(sides) and not is_isosceles(sides)
+
+def valid_triangle(sides):
+    return all(side != 0 for side in sides) and all([sides[idx[0]] + sides[idx[1]] >= sides[idx[2]]for idx in [(0,1,2), (1, 2,0), (2, 0, 1)]])
