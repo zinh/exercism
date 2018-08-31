e667e838970c9679b0f0b8ba888b42654f64b7d2
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-31 16:30:48 +0900

    ISBN verifier

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,12 +1,23 @@
 module IsbnVerifier (isbn) where
+import Data.Char (ord)
 
 isbn :: String -> Bool
-isbn s
-  | validDigit s = True
-  | otherwise = False
+isbn s = validDigit digits
+  where digits = normalize s
 
 normalize ::String -> Maybe String
-validDigit (c:str)
-  | c == '-' || c == 'X' = True
-  | c >= '0' && c <= '9' = True
+normalize isbn = [c | c <- isbn, c == 'X' || (c >= '0' && c <= '9')]
+
+validDigit :: String -> Bool
+validDigit digits
+  | length digits == 10 = checkDigits digits
   | otherwise = False
+
+checkDigits :: String -> Bool
+checkDigits digits 
+  | fst result `mod` 11 == 0 = True
+  | otherwise = False
+  where result = foldl (\memo digit -> (fst memo + snd memo * score digit, snd memo - 1)) (0, length digits) digits
+        score digit
+          | digit == 'X' = 10
+          | otherwise = (ord digit) - (ord '0')
