be696728b4fe5bae0f4ac6b50193efba4a06cef3
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-08-31 15:55:00 +0900

    Haskell: isbn verifier

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,12 @@
 module IsbnVerifier (isbn) where
 
 isbn :: String -> Bool
-isbn = error "You need to implement this function."
+isbn s
+  | validDigit s = True
+  | otherwise = False
+
+normalize ::String -> Maybe String
+validDigit (c:str)
+  | c == '-' || c == 'X' = True
+  | c >= '0' && c <= '9' = True
+  | otherwise = False
