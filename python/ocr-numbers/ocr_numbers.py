93a1b7aef790d580954a6f1098ca5918a6fb116a
Vinh Nguyen <xuanvinhct@gmail.com>
2018-09-01 17:20:26 +0900

    Python ocr numbers solution

--- python/ocr-numbers/ocr_numbers.py
+++ python/ocr-numbers/ocr_numbers.py
@@ -1,13 +1,31 @@
+height = 4
+width = 3
+
 def convert(input_grid):
-    # 011 1
-    # 001 222 010 2
-    # 222 011 3
-    # 010 020 011 4
-    # 010 222 001 5
-    # 011 222 001 6
-    # 200 011 7
-    # 011 222 011 8
-    # 010 222 011 9
-    # 011 202 011 0
-    # Using longest matching series
-    for col in range(0, len(input_grid)):
+    lines = [input_grid[i:i + height] for i in range(0, len(input_grid), height)]
+    return ','.join([convert_line(line) for line in lines])
+
+# line: array of string
+def convert_line(line):
+    #import pdb; pdb.set_trace()
+    line_width = len(line[0])
+    return "".join([analyze_number([row[col:col + width] for row in line]) for col in range(0, line_width, width)])
+
+# number: 3x4
+def analyze_number(number):
+    if len(number) != height or len(number[0]) != width:
+        raise ValueError('Invalid number')
+    m = {'0': [" _ ", "| |", "|_|"], '1': ["   ", "  |", "  |"], '2': [" _ ", " _|", "|_ "], 
+         '3': [" _ ", " _|", " _|"], '4': ["   ", "|_|", "  |"], '5': [" _ ", "|_ ", " _|"], 
+         '6': [" _ ", "|_ ", "|_|"], '7': [" _ ", "  |", "  |"], '8': [" _ ", "|_|", "|_|"], 
+         '9': [" _ ", "|_|", " _|"]}
+    for num, bitmaps in m.items():
+        if compare_bitmaps(number, bitmaps):
+            return num
+    return '?'
+
+def compare_bitmaps(number, bitmaps):
+    for idx, bitmap in enumerate(bitmaps):
+        if bitmap != number[idx]:
+            return False
+    return True
