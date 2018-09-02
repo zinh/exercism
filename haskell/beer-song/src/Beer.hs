a05ca4b09490d37b4a229d62c112c40d584cd97b
Vinh Nguyen <xuanvinhct@gmail.com>
2018-09-02 15:58:45 +0900

    fix

--- haskell/beer-song/src/Beer.hs
+++ haskell/beer-song/src/Beer.hs
@@ -15,6 +15,3 @@ beer :: Int -> Bool -> String
 beer n cap
   | n >= 2 = (show n) ++ " bottles"
   | n == 1 = "1 bottle"
-  | n == 0 && cap = "No more bottles"
-  | n == 9 && not cap = "no more bottles"
-  | otherwise = "99 bottles"
