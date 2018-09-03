2f3287592969bc005941b190168715e4fa836a22
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-03 10:49:07 +0900

    Tree building, refactor 1

--- python/tree-building/tree_building.py
+++ python/tree-building/tree_building.py
@@ -15,36 +15,49 @@ def BuildTree(records):
     records.sort(key=lambda x: x.record_id)
     ordered_id = [i.record_id for i in records]
     if records:
-        if ordered_id[-1] != len(ordered_id) - 1:
-            raise ValueError('Tree must be continuous')
-        if ordered_id[0] != 0:
-            raise ValueError('Tree must start with id 0')
+        ValidateTree(ordered_id)
+    else:
+        return None
     trees = []
     parent = {}
-    for i in range(len(ordered_id)):
-        for j in records:
-            if ordered_id[i] == j.record_id:
-                if j.record_id == 0:
-                    if j.parent_id != 0:
-                        raise ValueError('Root node cannot have a parent')
-                if j.record_id < j.parent_id:
-                    raise ValueError('Parent id must be lower than child id')
-                if j.record_id == j.parent_id:
-                    if j.record_id != 0:
-                        raise ValueError('Tree is a cycle')
-                trees.append(Node(ordered_id[i]))
-    for i in range(len(ordered_id)):
-        for j in trees:
-            if i == j.node_id:
-                parent = j
-        for j in records:
-            if j.parent_id == i:
-                for k in trees:
-                    if k.node_id == 0:
+    trees = CreateTree(ordered_id, records)
+    for record_id in range(len(ordered_id)):
+        for node in trees:
+            if record_id == node.node_id:
+                parent = node
+        for record in records:
+            if record.parent_id == record_id:
+                for node in trees:
+                    if node.node_id == 0:
                         continue
-                    if j.record_id == k.node_id:
-                        child = k
+                    if record.record_id == node.node_id:
+                        child = node
                         parent.children.append(child)
     if len(trees) > 0:
         root = trees[0]
     return root
+
+def ValidateTree(ordered_id):
+    if ordered_id[-1] != len(ordered_id) - 1:
+        raise ValueError('Tree must be continuous')
+    if ordered_id[0] != 0:
+        raise ValueError('Tree must start with id 0')
+
+def ValidateNode(node):
+    if node.record_id == 0:
+        if node.parent_id != 0:
+            raise ValueError('Root node cannot have a parent')
+    if node.record_id < node.parent_id:
+        raise ValueError('Parent id must be lower than child id')
+    if node.record_id == node.parent_id:
+        if node.record_id != 0:
+            raise ValueError('Tree is a cycle')
+
+def CreateTree(ordered_id, records):
+    trees = []
+    for record_id in range(len(ordered_id)):
+        for record in records:
+            if ordered_id[record_id] == record.record_id:
+                ValidateNode(record)
+                trees.append(Node(ordered_id[record_id]))
+    return trees
