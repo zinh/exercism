6061a2d38b5da7537481f4daa6fa1efa11b589a5
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-03 14:51:23 +0900

    Haskell triangle solution

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -6,5 +6,18 @@ data TriangleType = Equilateral
                   | Illegal
                   deriving (Eq, Show)
 
-triangleType :: Num a => a -> a -> a -> TriangleType
-triangleType a b c = error "You need to implement this function."
+triangleType :: (Num a, Ord a, Eq a) => a -> a -> a -> TriangleType
+triangleType a b c
+  | not (validTriangle a b c) || allEqual a b c && a == 0 = Illegal
+  | allEqual a b c = Equilateral
+  | twoEqual a b c = Isosceles
+  | otherwise = Scalene
+
+allEqual :: (Num a, Eq a) => a -> a -> a -> Bool
+allEqual a b c = a == b && b == c && c == a
+
+twoEqual :: (Num a, Eq a) => a -> a -> a -> Bool
+twoEqual a b c = a == b || b ==c || c == a
+
+validTriangle :: (Num a, Ord a) => a -> a -> a -> Bool
+validTriangle a b c = all ((<=) 0) [a + b - c, b + c - a, c + a - b]
