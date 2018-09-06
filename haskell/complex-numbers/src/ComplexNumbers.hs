d894e89e9115a400cf6bfb12b5f19172f889feab
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-06 18:49:31 +0900

    Haskell complex number solution

--- haskell/complex-numbers/src/ComplexNumbers.hs
+++ haskell/complex-numbers/src/ComplexNumbers.hs
@@ -13,33 +13,36 @@ module ComplexNumbers
 import Prelude hiding (div, abs)
 
 -- Data definition -------------------------------------------------------------
-data Complex a = Dummy deriving(Eq, Show)
+data Complex a = Complex a a deriving(Eq, Show)
 
 complex :: (a, a) -> Complex a
-complex = error "You need to implement this function"
+complex (r, m) = Complex r m
 
 -- unary operators -------------------------------------------------------------
 conjugate :: Num a => Complex a -> Complex a
-conjugate = error "You need to implement this function"
+conjugate (Complex a b) = Complex a (-b)
 
 abs :: Floating a => Complex a -> a
-abs = error "You need to implement this function"
+abs (Complex r i) = sqrt $ r ^ 2 + i ^ 2
 
 real :: Num a => Complex a -> a
-real = error "You need to implement this function"
+real (Complex r _) = r
 
 imaginary :: Num a => Complex a -> a
-imaginary = error "You need to implement this function"
+imaginary (Complex _ i) = i
 
 -- binary operators ------------------------------------------------------------
 mul :: Num a => Complex a -> Complex a -> Complex a
-mul = error "You need to implement this function"
+mul (Complex a b) (Complex c d) = Complex (a * c - b * d) (b * c + a * d)
 
 add :: Num a => Complex a -> Complex a -> Complex a
-add = error "You need to implement this function"
+add (Complex a b) (Complex c d) = Complex (a + c) (b + d)
 
 sub :: Num a => Complex a -> Complex a -> Complex a
-sub = error "You need to implement this function"
+sub (Complex a b) (Complex c d) = Complex (a - c) (b - d)
 
 div :: Fractional a => Complex a -> Complex a -> Complex a
-div = error "You need to implement this function"
+div (Complex a b) f@(Complex c d) = Complex r i
+  where l1 = c^2 + d^2
+        r = (a * c + b * d) / l1
+        i = (b * c - a * d) / l1
