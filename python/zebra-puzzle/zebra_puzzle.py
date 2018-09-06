7dc803d9b729e20666506e0a3de942456b289ae3
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-06 19:35:33 +0900

    Python zebra puzzle

--- python/zebra-puzzle/zebra_puzzle.py
+++ python/zebra-puzzle/zebra_puzzle.py
@@ -1,3 +1,16 @@
+houses = ['red', 'green', 'ivory', 'yellow', 'blue']
+inhabitants = ['Englishman', 'Ukrainian', 'Norwegian', 'Japanese', 'Spaniard']
+pets = ['dog', 'snails', 'fox', 'horse', '']
+drinks = ['coffee', 'tea', 'milk', 'orange juice', '']
+smoke = ['Old Gold', 'Kools', 'Chesterfields', 'Lucky Strike', '']
+
+predicates = [['Englishman', 'red'], ['Spaniard', 'dog'], 
+        ['coffee', 'green'], ['Ukrainian', 'tea'],
+        ['Old Gold','snails'], ['Kools','yellow']
+        ['Lucky Strike','orange juice'], ['Japanese','Parliaments']
+        ['',''], ['','']
+        ]
+
 def drinks_water():
     pass
 
