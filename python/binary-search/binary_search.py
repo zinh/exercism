0a44b2b84cdda85652d32d6db91a384c6edf5c2a
Vinh Nguyen <xuanvinhct@gmail.com>
2018-09-07 21:43:21 +0900

    Python binary search solution

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,15 +1,16 @@
-from math import ceil
-
 def binary_search(list_of_numbers, number):
-    if len(list_of_numbers) == 0:
+    if len(list_of_numbers) == 0 or number < list_of_numbers[0] or number > list_of_numbers[-1]:
         raise ValueError("Not found")
+    if len(list_of_numbers) == 1 and list_of_numbers[0] != number:
+        raise ValueError("Not found")
+    if len(list_of_numbers) == 1 and list_of_numbers[0] == number:
+        return 0
     return binary_search_recur(list_of_numbers, number, 0, len(list_of_numbers))
 
 def binary_search_recur(list_of_numbers, number, lower, upper):
-    if upper <= lower:
+    if upper <= lower or ((upper - lower) == 1 and list_of_numbers[upper] != number and list_of_numbers[lower] != number):
         raise ValueError("Not found")
-    print(lower, upper)
-    mid = ceil((upper + lower) / 2)
+    mid = (upper + lower) // 2
     if mid >= len(list_of_numbers):
         raise ValueError("Not found")
     if list_of_numbers[mid] == number:
@@ -18,5 +19,3 @@ def binary_search_recur(list_of_numbers, number, lower, upper):
         return binary_search_recur(list_of_numbers, number, lower, mid)
     else:
         return binary_search_recur(list_of_numbers, number, mid, upper)
-
-print(binary_search([1, 3, 4, 6, 8, 9, 11], 0))
