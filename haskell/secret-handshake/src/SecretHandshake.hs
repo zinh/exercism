99fc1b6e56c92423a78247cb61d0628dc675763c
Vinh Nguyen <xuanvinhct@gmail.com>
2018-09-08 11:46:27 +0900

    Haskell secret handshake solution

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,10 @@
 module SecretHandshake (handshake) where
 
+import Data.Bits((.&.))
+import qualified Data.Map as Map
+
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake 0 = []
+handshake n = if (.&.) n 16 > 0 then reverse actions else actions
+  where m = Map.fromList [(1, "wink"), (2, "double blink"), (4, "close your eyes"), (8, "jump")]
+        actions = foldr (\b memo -> if (.&.) n b > 0 then (Map.findWithDefault "A" b m):memo else memo) [] [1, 2, 4, 8]
