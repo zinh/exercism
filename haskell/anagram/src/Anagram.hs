2ec1fecfdf5205103c2cf61d378a82a67782365e
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-12 11:21:43 +0900

    Haskell anagram

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,4 +1,9 @@
 module Anagram (anagramsFor) where
+import Data.List (sort)
+import Data.Char (toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor xs xss = foldr (\str result -> if sortStr str == sorted && normalized str /= normalized xs then str:result else result) [] xss
+  where normalized str = map toLower str
+        sortStr = sort . normalized
+        sorted = sortStr xs
