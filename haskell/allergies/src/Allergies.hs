baaf69b852051319778471598d06dc5734c8cd71
Vinh Nguyen <xuanvinhct@gmail.com>
2018-09-23 11:20:43 +0900

    Haskell allergies solution

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -1,5 +1,8 @@
 module Allergies (Allergen(..), allergies, isAllergicTo) where
 
+import Data.Bits ((.&.))
+import Data.Map.Strict (fromList, findWithDefault, keys)
+
 data Allergen = Eggs
               | Peanuts
               | Shellfish
@@ -8,10 +11,15 @@ data Allergen = Eggs
               | Chocolate
               | Pollen
               | Cats
-              deriving (Eq)
+              deriving (Eq, Ord)
+
+m = fromList [(Eggs, 1), (Peanuts, 2), (Shellfish, 4), (Strawberries, 8), (Tomatoes, 16), (Chocolate, 32), (Pollen, 64), (Cats, 128)] 
+
+allergiesMap :: Allergen -> Int
+allergiesMap allergen = findWithDefault 0 allergen m
 
 allergies :: Int -> [Allergen]
-allergies score = error "You need to implement this function."
+allergies score = filter (`isAllergicTo` score) (keys m)
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = error "You need to implement this function."
+isAllergicTo allergen score = (.&.) score (allergiesMap allergen) > 0
