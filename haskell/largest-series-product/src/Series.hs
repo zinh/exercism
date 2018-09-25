7bec48d3ba3e2ef389ab9e7d01d0197d5d54adc2
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-09-25 11:00:52 +0900

    Largest series product solution

--- haskell/largest-series-product/src/Series.hs
+++ haskell/largest-series-product/src/Series.hs
@@ -1,6 +1,22 @@
 module Series (Error(..), largestProduct) where
 
+import Data.Char (isNumber, digitToInt)
+
 data Error = InvalidSpan | InvalidDigit Char deriving (Show, Eq)
 
-largestProduct :: Int -> String -> Either Error Integer
-largestProduct size digits = error "You need to implement this function."
+largestProduct :: Int -> String -> Either Error Int
+largestProduct size digits
+  | size == 0 = Right 1
+  | size > length digits || size < 0 = Left InvalidSpan
+  | any notANumber digits = Left $ InvalidDigit (head $ filter notANumber digits)
+  | otherwise = Right $ maximum $ map (largestProduct' size) (subString size digits)
+  where notANumber = not . isNumber
+
+largestProduct' :: Int -> String -> Int
+largestProduct' 1 (digit:digits) = digitToInt digit
+largestProduct' size (digit:digits) = digitToInt digit * (largestProduct' (size - 1) digits)
+
+subString :: Int -> [a] -> [[a]]
+subString lengthLimit l@(x:xs)
+  | length l <= lengthLimit = [l]
+  | otherwise = l:(subString lengthLimit xs)
