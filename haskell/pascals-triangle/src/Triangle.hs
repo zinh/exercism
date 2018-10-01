51f35e145d9f2df9672af0718ee455a52392428a
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-01 11:36:19 +0900

    Haskell passcals triangle: solution

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,18 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows x = (reverse . rows') x
+
+rows' :: Int -> [[Integer]]
+rows' x
+  | x <= 0 = []
+rows' 1 = [[1]]
+rows' 2 = [[1, 1], [1]]
+rows' x =
+  let triangle@(r:_) = rows' (x - 1)
+  in (1:(nextRow r)) : triangle
+
+nextRow :: [Integer] -> [Integer]
+nextRow [] = []
+nextRow (x:[]) = [1]
+nextRow (x1:xs@(x2:_)) = (x1 + x2) : (nextRow xs)
