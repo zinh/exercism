37b2226465bd04496747d97993fa7ecc8356046b
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-01 11:19:21 +0900

    Python: pretein translation solution

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,9 @@
-def proteins(strand):
-    pass
+from itertools import takewhile
+m = {'AUG': 'Methionine', 'UUU': 'Phenylalanine', 'UUC': 'Phenylalanine',
+        'UUA': 'Leucine', 'UUG': 'Leucine', 'UCU': 'Serine', 'UCC': 'Serine',
+        'UCA': 'Serine', 'UCG': 'Serine', 'UAU': 'Tyrosine', 'UAC': 'Tyrosine',
+        'UGU': 'Cysteine', 'UGC': 'Cysteine', 'UGG': 'Tryptophan', 
+        'UAA': 'STOP', 'UAG': 'STOP', 'UGA': 'STOP'}
+
+def proteins(strand):                                            
+    return list(takewhile(lambda s: s!= 'STOP', [m[strand[pos:pos+3]] for pos in range(0, len(strand), 3)]))
