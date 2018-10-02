4f09b12dcc79c98e5f50006628aa08ee6a437598
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-02 18:30:22 +0900

    Python: scale generator

--- python/scale-generator/scale_generator.py
+++ python/scale-generator/scale_generator.py
@@ -1,13 +1,22 @@
-class Scale(object):
-    def __init__(self, tonic, intervals="mmmmmmmmmmmm"):
-        self.pitches = self.generate_pitches(tonic, intervals)
+notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 
-    def generate_pitches(self, tonic, intervals="mmmmmmmmmmmm"):
-        notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+class Scale(object):
+    def __init__(self, tonic, intervals="mmmmmmmmmmm"):
+        self.pitches = self.generate_pitches(tonic.upper(), intervals)
+
+    def generate_pitches(self, tonic, intervals="mmmmmmmmmmm"):
         steps = {'m': 1, 'M': 2, 'A': 3}
-        start_idx = notes.index(tonic.upper())
-        results = [tonic.upper()]
+        start_idx = notes.index(tonic)
+        results = [tonic]
         for interval in intervals:
             start_idx += steps[interval]
-            results.append(notes[start_idx % len(notes)])
+            note = notes[start_idx % len(notes)]
+            if note != tonic:
+                results.append(note)
         return results
+
+    def convert_flat(flat_note):
+        note = flat_note[0]
+        idx = notes.index(note)
+        sharp_note = idx - 1
+        return notes[sharp_note]
