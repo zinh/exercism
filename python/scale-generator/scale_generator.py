854be3d95b248593d849d2c040c72b61c17e1d2d
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-02 16:31:58 +0900

    Python: scale generator

--- python/scale-generator/scale_generator.py
+++ python/scale-generator/scale_generator.py
@@ -1,3 +1,13 @@
 class Scale(object):
-    def __init__(self, tonic, intervals=None):
-        self.pitches = None
+    def __init__(self, tonic, intervals="mmmmmmmmmmmm"):
+        self.pitches = self.generate_pitches(tonic, intervals)
+
+    def generate_pitches(self, tonic, intervals="mmmmmmmmmmmm"):
+        notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+        steps = {'m': 1, 'M': 2, 'A': 3}
+        start_idx = notes.index(tonic.upper())
+        results = [tonic.upper()]
+        for interval in intervals:
+            start_idx += steps[interval]
+            results.append(notes[start_idx % len(notes)])
+        return results
