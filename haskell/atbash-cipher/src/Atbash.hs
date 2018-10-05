7dc84ce448e4f0f7c21fca5feca07531d13837fb
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-05 10:48:27 +0900

    Haskell: atbash cipher solution

--- haskell/atbash-cipher/src/Atbash.hs
+++ haskell/atbash-cipher/src/Atbash.hs
@@ -1,7 +1,27 @@
 module Atbash (decode, encode) where
 
+import Data.List (intercalate)
+import Data.Char (ord, chr, toLower, isAlphaNum, isNumber)
+import Data.List.Split (chunksOf)
+
 decode :: String -> String
-decode cipherText = error "You need to implement this function."
+decode cipherText = map decodeChar (filter isAlphaNum cipherText)
 
 encode :: String -> String
-encode plainText = error "You need to implement this function."
+encode plainText = (intercalate " " . chunksOf 5 . map encodeChar) (filter isAlphaNum plainText)
+
+encodeChar :: Char -> Char
+encodeChar c
+  | isNumber c = c
+encodeChar c =
+  let n = (ord . toLower) c
+      distanceFromA = n - ord 'a'
+  in chr $ ord 'z' - distanceFromA
+
+decodeChar :: Char -> Char
+decodeChar c
+  | isNumber c = c
+decodeChar c =
+  let n = (ord . toLower) c
+      distanceFromZ = ord 'z' - n
+  in chr $ ord 'a' + distanceFromZ
