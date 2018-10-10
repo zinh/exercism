7dc900ce77db7fec08f879034ea91335f25e843d
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-10 13:11:55 +0900

    Python complex numbers: solution

--- python/complex-numbers/complex_numbers.py
+++ python/complex-numbers/complex_numbers.py
@@ -1,24 +1,44 @@
+from cmath import sqrt, exp, sin, cos
+from math import pi
+
 class ComplexNumber(object):
     def __init__(self, real, imaginary):
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __add__(self, other):
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __mul__(self, other):
-        pass
+        r = self.real * other.real - self.imaginary * other.imaginary
+        i = self.real * other.imaginary + self.imaginary * other.real
+        return ComplexNumber(r, i)
 
     def __sub__(self, other):
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __truediv__(self, other):
-        pass
+        norminator = self * other.conjugate()
+        denominator = other * other.conjugate()
+        return ComplexNumber(norminator.real / denominator.real, norminator.imaginary / denominator.real)
 
     def __abs__(self):
-        pass
+        return sqrt(self.real**2 + self.imaginary**2)
+
+    def __eq__(self, other):
+        return self.real == other.real and self.imaginary == other.imaginary
 
     def conjugate(self):
-        pass
+        return ComplexNumber(self.real, -self.imaginary)
 
     def exp(self):
-        pass
+        ex = exp(self.real)
+        if self.imaginary == pi:
+            return ComplexNumber(ex * cos(self.imaginary), 0)
+        return ComplexNumber(ex * cos(self.imaginary), ex * sin(self.imaginary))
+
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+
+    def __repr__(self):
+        return f"{self.real} + {self.imaginary}i"
