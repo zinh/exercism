9a07f6ba41bc9e87a457d3a83ae23844ab4bd478
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-11 15:27:27 +0900

    Haskell binary search tree: solution

--- haskell/binary-search-tree/src/BST.hs
+++ haskell/binary-search-tree/src/BST.hs
@@ -10,28 +10,35 @@ module BST
     , toList
     ) where
 
-data BST a = Dummy deriving (Eq, Show)
+data BST a = Empty | Tree a (BST a) (BST a) deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft tree = error "You need to implement this function."
+bstLeft (Tree _ Empty _) = Nothing
+bstLeft (Tree _ left _) = Just left
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight tree = error "You need to implement this function."
+bstRight (Tree _ _ Empty) = Nothing
+bstRight (Tree _ _ right) = Just right
 
 bstValue :: BST a -> Maybe a
-bstValue tree = error "You need to implement this function."
+bstValue Empty = Nothing
+bstValue (Tree value _ _) = Just value
 
 empty :: BST a
-empty = error "You need to implement this function."
+empty = Empty
 
 fromList :: Ord a => [a] -> BST a
-fromList xs = error "You need to implement this function."
+fromList = foldl (\t x -> insert x t) Empty
 
 insert :: Ord a => a -> BST a -> BST a
-insert x tree = error "You need to implement this function."
+insert x Empty = Tree x Empty Empty
+insert x t@(Tree value left right)
+  | x <= value = Tree value (insert x left) right
+  | otherwise = Tree value left (insert x right)
 
 singleton :: a -> BST a
-singleton x = error "You need to implement this function."
+singleton x = Tree x Empty Empty
 
 toList :: BST a -> [a]
-toList tree = error "You need to implement this function."
+toList Empty = []
+toList (Tree value left right) = (toList left) ++ [value] ++ (toList right)
