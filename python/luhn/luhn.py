faed8d1da51e8a90ddc0d940a735bb3c855e0957
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-12 10:34:28 +0900

    Python: luhn

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,20 @@
 class Luhn(object):
     def __init__(self, card_num):
-        pass
+        self.card_num = []
+        for num in card_num:
+            if num != ' ' and (num < '0' or num > '9'):
+                self.card_num = []
+                break
+            elif num >= '0' and num <= '9':
+                self.card_num.append(ord(num) - ord('0'))
+            else:
+                continue
+        print(self.card_num)
 
     def is_valid(self):
-        pass
+        if len(self.card_num) <= 1:
+            return False
+        s = sum([num if pos % 2 == 0 else num * 2 if num <= 4 else num * 2 - 9 for pos, num in enumerate(self.card_num[::-1])])
+        return s % 10 == 0
+
+#l = Luhn("59")
