22c0cbe41165ce4d905382d88d2de6641a1ce83f
Vinh Nguyen <xuanvinhct@gmail.com>
2018-10-14 22:17:01 +0900

    Haskell ocr

--- haskell/ocr-numbers/src/OCR.hs
+++ haskell/ocr-numbers/src/OCR.hs
@@ -1,4 +1,23 @@
 module OCR (convert) where
 
+import Data.List.Split (chunksOf)
+
+m :: [(Int, [String])]
+m =  [('0', [" _ ", "| |", "|_|"]), ('1', ["   ", "  |", "  |"]), ('2', [" _ ", " _|", "|_ "]), 
+         ('3', [" _ ", " _|", " _|"]), ('4', ["   ", "|_|", "  |"]), ('5', [" _ ", "|_ ", " _|"]), 
+         ('6', [" _ ", "|_ ", "|_|"]), ('7', [" _ ", "  |", "  |"]), ('8', [" _ ", "|_|", "|_|"]), 
+         ('9', [" _ ", "|_|", " _|"])]
+
+matchNumber :: [String] -> Char
+matchNumber rows =
+  let compareBitmap (number, bitmaps) = all (\(row, bitmap) -> row == bitmap) (zip rows bitmaps)
+      matched = filter compareBitmap m
+   in case matched of
+        [] -> "?"
+        (number, _):_ -> show number
+
 convert :: String -> String
-convert xs = error "You need to implement this function."
+convert input =
+  let xs = lines input
+      map (\rows -> [chunksOf 3 row | row <- rows]) (chunksOf 4 xs)
+   in matchedNumber
