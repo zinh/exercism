664e67eab438b6d46d1ece4d458bb4b70cc93b3e
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-15 11:05:46 +0900

    Haskell: OCR number solution

--- haskell/ocr-numbers/src/OCR.hs
+++ haskell/ocr-numbers/src/OCR.hs
@@ -1,8 +1,9 @@
 module OCR (convert) where
 
 import Data.List.Split (chunksOf)
+import Data.List (intercalate)
 
-m :: [(Int, [String])]
+m :: [(Char, [String])]
 m =  [('0', [" _ ", "| |", "|_|"]), ('1', ["   ", "  |", "  |"]), ('2', [" _ ", " _|", "|_ "]), 
          ('3', [" _ ", " _|", " _|"]), ('4', ["   ", "|_|", "  |"]), ('5', [" _ ", "|_ ", " _|"]), 
          ('6', [" _ ", "|_ ", "|_|"]), ('7', [" _ ", "  |", "  |"]), ('8', [" _ ", "|_|", "|_|"]), 
@@ -13,11 +14,17 @@ matchNumber rows =
   let compareBitmap (number, bitmaps) = all (\(row, bitmap) -> row == bitmap) (zip rows bitmaps)
       matched = filter compareBitmap m
    in case matched of
-        [] -> "?"
-        (number, _):_ -> show number
+        [] -> '?'
+        (number, _):_ -> number
 
 convert :: String -> String
+--convert xs = error "Not implemented"
 convert input =
   let xs = lines input
-      map (\rows -> [chunksOf 3 row | row <- rows]) (chunksOf 4 xs)
+      matchedNumber = intercalate "," $ map (cutRows matchNumber) (chunksOf 4 xs)
    in matchedNumber
+
+cutRows :: ([[a]] -> b) -> [[a]] -> [b]
+cutRows _ rows
+  | all null rows = []
+cutRows f rows = f (map (take 3) rows) : cutRows f (map (drop 3) rows)
