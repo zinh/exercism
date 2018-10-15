8ab68892b9927ef0ed41eaad929e55390fc2f388
Vinh Nguyen <xuanvinhct@gmail.com>
2018-10-15 22:27:50 +0900

    Haskell: rotation cipher solution

--- haskell/rotational-cipher/src/RotationalCipher.hs
+++ haskell/rotational-cipher/src/RotationalCipher.hs
@@ -1,4 +1,12 @@
 module RotationalCipher (rotate) where
 
+import Data.Char (isAlpha, isAscii, ord, chr)
+
 rotate :: Int -> String -> String
-rotate = error "You need to implement this function."
+rotate n str = map (encrypt n) str
+
+encrypt :: Int -> Char -> Char
+encrypt n c
+  | c >= 'a' && c <= 'z' = chr $ ord 'a' + ((ord c) - (ord 'a') + n) `mod` 26
+  | c >= 'A' && c <= 'Z' = chr $ ord 'A' + ((ord c) - (ord 'A') + n) `mod` 26
+  | otherwise = c
