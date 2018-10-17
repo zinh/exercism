01af89b7b4975be20dd90904731c2bde9b465953
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-17 10:06:18 +0900

    Spiral matrix

--- haskell/spiral-matrix/src/Spiral.hs
+++ haskell/spiral-matrix/src/Spiral.hs
@@ -1,4 +1,7 @@
 module Spiral (spiral) where
 
+import Data.Array (Array, listArray)
+
 spiral :: Int -> [[Int]]
-spiral size = error "You need to implement this function."
+spiral 0 = []
+spiral n = spiralArray listArray (0, n - 1) [listArray (0, n - 1) [0 | j <- [0..(n - 1)]] | i <- [0..(n - 1)]]
