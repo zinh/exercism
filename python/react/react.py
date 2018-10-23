3c8bc4e2421e0d5aa324cc81cc8858325226dd07
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-23 16:15:04 +0900

    Python: react

--- python/react/react.py
+++ python/react/react.py
@@ -1,11 +1,19 @@
 class InputCell(object):
     def __init__(self, initial_value):
-        self.value = None
+        self._value = initial_value
+
+    def set_value(self, value):
+        self._value = value
+
+    def get_value(self):
+        return self._value
+
+    value = property(get_value, set_value)
 
 
 class ComputeCell(object):
     def __init__(self, inputs, compute_function):
-        self.value = None
+        self.value = compute_function(inputs)
 
     def add_callback(self, callback):
         pass
