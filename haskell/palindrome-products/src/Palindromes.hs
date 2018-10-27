af210065ba9f063d4979fcbee5a6c2d6bd16b406
Vinh Nguyen <xuanvinhct@gmail.com>
2018-10-27 22:22:31 +0900

    Haskell: palindrome products

--- haskell/palindrome-products/src/Palindromes.hs
+++ haskell/palindrome-products/src/Palindromes.hs
@@ -1,7 +1,17 @@
 module Palindromes (largestPalindrome, smallestPalindrome) where
 
+import Data.Ord (comparing)
+import Data.Foldable (maximumBy)
+
 largestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-largestPalindrome minFactor maxFactor = error "You need to implement this function."
+largestPalindrome minFactor maxFactor = 
+  let p = maximumBy (comparing fst) (filter (isPalindrome . fst) [(i * j, (i, j)) | i <- [maxFactor, (maxFactor - 1)..minFactor], j <- [i, (i - 1)..minFactor]])
+   in (fst p, [snd p])
 
 smallestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-smallestPalindrome minFactor maxFactor = error "You need to implement this function."
+smallestPalindrome minFactor maxFactor = 
+  let p = head (filter (isPalindrome . fst) [(i * j, (i, j)) | i <- [minFactor..maxFactor], j <- [minFactor..i]])
+   in (fst p, [snd p])
+
+isPalindrome :: Integer -> Bool
+isPalindrome n = show n == (reverse . show) n
