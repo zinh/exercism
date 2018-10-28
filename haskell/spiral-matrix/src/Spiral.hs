563349e93f9f2134e1e1729ee8a3c369984ebcb0
Vinh Nguyen <xuanvinhct@gmail.com>
2018-10-28 13:37:26 +0900

    Haskell: spiral matrix

--- haskell/spiral-matrix/src/Spiral.hs
+++ haskell/spiral-matrix/src/Spiral.hs
@@ -1,7 +1,20 @@
 module Spiral (spiral) where
 
-import Data.Array (Array, listArray)
+--import Data.Array (Array, listArray)
 
 spiral :: Int -> [[Int]]
 spiral 0 = []
-spiral n = spiralArray listArray (0, n - 1) [listArray (0, n - 1) [0 | j <- [0..(n - 1)]] | i <- [0..(n - 1)]]
+
+spiral :: Int -> Int -> [[Int]]
+spiral size topLeft n =
+  let topRight = topLeft + n - 1
+      bottomRight = 2 * (n - 1) + 1
+      bottomLeft = bottomRight + n - 1
+      innerMatrix = spiral (n - 1)
+      firstRow = [i | i <- [topLeft..topRight]]
+      lastRow = reverse [i | i <- [bottomRight..bottomLeft]]
+   in firstRow : (appendNumber (topRight + 1) innerMatrix) ++ [lastRow]
+
+appendNumber :: Int -> Int -> [[Int]] -> [[Int]]
+appendNumber _ _ [] = []
+appendNumber h t (xs:xss) = ((h : xs) ++ [t]) : appendNumber (h + 1) (t - 1) xs
