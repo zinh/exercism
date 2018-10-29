6f6d0bbd651a8da7320724e61de85d65bc196198
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-10-29 19:06:12 +0900

    Haskell: palindrome

--- haskell/palindrome-products/src/Palindromes.hs
+++ haskell/palindrome-products/src/Palindromes.hs
@@ -4,14 +4,21 @@ import Data.Ord (comparing)
 import Data.Foldable (maximumBy)
 
 largestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-largestPalindrome minFactor maxFactor = 
-  let p = maximumBy (comparing fst) (filter (isPalindrome . fst) [(i * j, (i, j)) | i <- [maxFactor, (maxFactor - 1)..minFactor], j <- [i, (i - 1)..minFactor]])
-   in (fst p, [snd p])
+largestPalindrome minFactor maxFactor = (p, factor p minFactor maxFactor)
+  where upper = maxFactor * maxFactor
+        lower = minFactor * minFactor
+        p = (head . (filter isPalindrome)) [i | i <- [upper, (upper - 1)..lower]]
 
 smallestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
-smallestPalindrome minFactor maxFactor = 
-  let p = head (filter (isPalindrome . fst) [(i * j, (i, j)) | i <- [minFactor..maxFactor], j <- [minFactor..i]])
-   in (fst p, [snd p])
+smallestPalindrome minFactor maxFactor = (p, factor p minFactor maxFactor)
+  where upper = maxFactor * maxFactor
+        lower = minFactor * minFactor
+        p = (head . (filter isPalindrome)) [i | i <- [lower..upper]]
 
 isPalindrome :: Integer -> Bool
 isPalindrome n = show n == (reverse . show) n
+
+factor :: Integer -> Integer -> Integer -> [(Integer, Integer)]
+factor n lower upper = [(i, j) | i <- [realLower..realUpper], j <- [i..realUpper], i * j == n]
+  where realLower = n `div` upper
+        realUpper = n `div` lower
