e5e5eef7a4797daec4918dec6c6e36cfbcfef1fd
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-11-02 18:34:12 +0900

    Haskell: sublist

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -3,4 +3,12 @@ module Sublist (Sublist(..), sublist) where
 data Sublist = Equal | Sublist | Superlist | Unequal deriving (Eq, Show)
 
 sublist :: [a] -> [a] -> Sublist
-sublist xs ys = error "You need to implement this function."
+sublist [] [] = Equal
+sublist [] _ = Sublist
+sublist _ [] = Superlist
+sublist xs ys = error ""
+sublist xss@(x:xs) yss@(y:ys)
+  | x == y = sublist xs ys
+  | otherwise = sublist xss ys
+  where s1 = sublist xss ys
+        s2 = sublist xs yss
