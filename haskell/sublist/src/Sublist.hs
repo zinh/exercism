f6dcc526ee88bfbbece4258d5bcb9b9c5a282395
Vinh Nguyen <vinhnx@zigexn.co.jp>
2018-11-02 19:03:18 +0900

    Haskell: sublist solution

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -2,13 +2,31 @@ module Sublist (Sublist(..), sublist) where
 
 data Sublist = Equal | Sublist | Superlist | Unequal deriving (Eq, Show)
 
-sublist :: [a] -> [a] -> Sublist
+sublist :: Eq a => [a] -> [a] -> Sublist
 sublist [] [] = Equal
 sublist [] _ = Sublist
 sublist _ [] = Superlist
-sublist xs ys = error ""
-sublist xss@(x:xs) yss@(y:ys)
-  | x == y = sublist xs ys
-  | otherwise = sublist xss ys
-  where s1 = sublist xss ys
-        s2 = sublist xs yss
+sublist xs ys
+  | s1 && s2 = Equal
+  | s1 = Sublist
+  | s2 = Superlist
+  | otherwise = Unequal
+  where s1 = xs `sublistOf` ys
+        s2 = ys `sublistOf` xs
+
+
+sublistOf :: Eq a => [a] -> [a] -> Bool
+sublistOf [] [] = True
+sublistOf _ [] = False
+sublistOf [] _ = False
+sublistOf xs yss@(y:ys)
+  | sublistOf' xs yss = True
+  | otherwise = sublistOf xs ys
+
+sublistOf' :: Eq a => [a] -> [a] -> Bool
+sublistOf' [] [] = True
+sublistOf' _ [] = False
+sublistOf' [] _ = True
+sublistOf' (x:xs) (y:ys)
+  | x == y = sublistOf' xs ys
+  | otherwise = False
