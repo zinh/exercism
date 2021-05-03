01653753435ab19f45b7d29c195525e1ab143770
Vinh Nguyen <zinh@users.noreply.github.com>
2021-05-03 21:03:52 +0900

    dart: word count(solution)

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,3 +1,15 @@
 class WordCount {
-  // Put your code here
+  Map<String, int> countWords(String s) {
+    final exp = RegExp(r"([a-zA-Z0-9]+('[a-zA-Z0-9]+)*)");
+    var m = <String, int>{};
+
+    for (var word in exp.allMatches(s.toLowerCase()).map((match) => match[0])) {
+      if (m.containsKey(word)) {
+        m[word] += 1;
+      } else {
+        m[word] = 1;
+      }
+    }
+    return m;
+  }
 }
