f058d801076137be9aa8bdbe1c974ff5883a565c
Vinh Nguyen <zinh@users.noreply.github.com>
2021-05-05 14:02:14 +0900

    dart: space age(solution)

--- dart/space-age/lib/space_age.dart
+++ dart/space-age/lib/space_age.dart
@@ -1,5 +1,18 @@
 class SpaceAge {
+  final ages = <String, double>{
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Earth': 1,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132
+  };
+
   double age({String planet, int seconds}) {
-    return 0;
+    final int secondsInYear = 31557600;
+    var earthYear = ages[planet];
+    return double.parse((seconds / (secondsInYear * earthYear)).toStringAsFixed(2));
   }
 }
