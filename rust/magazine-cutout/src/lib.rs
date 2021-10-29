f10c35deb48e80c3245bbc2c8e097a723104ea01
Vinh Nguyen <zinh@users.noreply.github.com>
2021-10-29 16:11:55 +0900

    update

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,14 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut cache: HashMap<&str, u32> = HashMap::new();
+    for &word in magazine {
+        if cache.contains_key(word) {
+        } else {
+            cache.insert(word, 1);
+        }
+    }
+    for &word in note {
+    }
+    true
 }
