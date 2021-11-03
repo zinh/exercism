4a1b07f5d660d515345aeca65d7138b34f4a9d6d
Vinh Nguyen <zinh@users.noreply.github.com>
2021-11-03 15:01:30 +0900

    giga seconds

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-extern crate chrono;
-use chrono::*;
+use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
-pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
+pub fn after(start_date: DateTime) -> DateTime {
     start_date + Duration::seconds(1_000_000_000)
 }
