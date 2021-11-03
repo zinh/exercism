c11b0afa429e3ea948aa7b9f38383eff797cdce3
User Vinh <vinh@www491.sakura.ne.jp>
2021-11-03 14:36:48 +0900

    add clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -8,28 +8,27 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock { hours, minutes }
+        let mut new_hours = hours;
+        let mut new_minutes = minutes;
+        if minutes < 0 {
+            new_minutes = minutes.rem_euclid(60);
+            new_hours += minutes.div_euclid(60);
+        }
+        if new_hours < 0 {
+            new_hours = new_hours.rem_euclid(24);
+        }
+        if new_minutes >= 60 {
+            new_hours = new_hours + new_minutes.div_euclid(60);
+            new_minutes = new_minutes.rem_euclid(60);
+        }
+        if new_hours >= 24 {
+            new_hours = new_hours.rem_euclid(24);
+        }
+        Clock{ hours: new_hours, minutes: new_minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let new_minutes = self.minutes + minutes;
-        if new_minutes < 60 {
-            return Clock {
-                hours: self.hours,
-                minutes: new_minutes,
-            };
-        }
-        let new_hours = self.hours + (new_minutes / 60);
-        if new_hours < 24 {
-            return Clock {
-                hours: new_hours,
-                minutes: new_minutes % 60,
-            };
-        }
-        Clock {
-            hours: new_hours % 24,
-            minutes: new_minutes % 60,
-        }
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
---
+++ rust/clock/src/main.rs
@@ -0,0 +1,6 @@
+use clock::Clock;
+
+fn main(){
+    println!("{}", Clock::new(25, 160));
+    println!("{}", (-268i32).div_euclid(60));
+}
