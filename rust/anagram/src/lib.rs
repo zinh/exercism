3ceb5956922353bb50a427081247d889afe41108
Vinh Nguyen <zinh@users.noreply.github.com>
2021-11-07 08:49:18 +0900

    anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,19 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut h: HashSet<&str> = HashSet::new();
+    let lower_word = word.to_lowercase();
+    for &possible_anagram in possible_anagrams {
+        if is_anagrams(possible_anagram, &lower_word) {
+            h.insert(possible_anagram);
+        }
+    }
+    h
+}
+
+fn is_anagrams(word: &str, target_word: &str) -> bool {
+    let mut word_vec : Vec<char> = word.chars().collect();
+    let mut target_word_vec : Vec<char> = target_word.chars().collect();
+    word_vec.sort_unstable();
+    target_word_vec.sort_unstable();
 }
